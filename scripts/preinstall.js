3492d1d8545259ae95a345303eb877de83a48f77
gotha331 <gotha331@gmail.com>
2024-05-14 20:32:24 +0800

    feat: force use of pnpm

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -3,7 +3,7 @@ import process from 'process'
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
   console.warn(
     `\u001b[33mThis repository must using pnpm as the package manager ` +
-    ` for scripts to work properly.\u001b[39m\n`,
+      ` for scripts to work properly.\u001b[39m\n`,
   )
   process.exit(1)
 }
