1cccad876a515e932988548ca84f863e586e7e0f
gotha331 <gotha331@gmail.com>
2024-05-15 10:01:38 +0800

    feat: add mock serve

--- vite.config.ts
+++ vite.config.ts
@@ -7,55 +7,62 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { viteMockServe } from 'vite-plugin-mock'
 
 const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': pathSrc,
-    },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-      imports: ['vue'],
-      resolvers: [
-        ElementPlusResolver(),
-        // 自动导入图标组件
-        // IconsResolver({
-        //   prefix: 'Icon',
-        // }),
-      ],
-      // dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
-    }),
-    Components({
-      resolvers: [
-        // 自动注册图标组件
-        // IconsResolver({
-        //   enabledCollections: ['ep'],
-        // }),
-        ElementPlusResolver(),
-      ],
-      // dts: path.resolve(pathSrc, 'components.d.ts'),
-    }),
-    // Icons({
-    //   autoInstall: true,
-    // }),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  css: {
-    //scss全局变量配置
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+export default defineConfig(({ command }) => {
+  return {
+    resolve: {
+      alias: {
+        '@': pathSrc,
       },
     },
-  },
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ['vue'],
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          // IconsResolver({
+          //   prefix: 'Icon',
+          // }),
+        ],
+        // dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
+      }),
+      Components({
+        resolvers: [
+          // 自动注册图标组件
+          // IconsResolver({
+          //   enabledCollections: ['ep'],
+          // }),
+          ElementPlusResolver(),
+        ],
+        // dts: path.resolve(pathSrc, 'components.d.ts'),
+      }),
+      // Icons({
+      //   autoInstall: true,
+      // }),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: command === 'serve',
+      }),
+    ],
+    css: {
+      //scss全局变量配置
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";',
+        },
+      },
+    },
+  }
 })
